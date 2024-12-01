7eef6f78d8b0665933f30b5f70b6a33ee3582e15
Neel Rao <neelorao@gmail.com>
2024-11-30 22:55:52 -0800

    feat: setup puzzle 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import kleur from 'kleur';
-import { puzzle1 } from '~/puzzles';
+import {
+    puzzle1,
+    puzzle2,
+} from '~/puzzles';
 import { Timer } from '~/util/Timer';
 
-async function start() {
+async function start() {    
     const timer = new Timer();
 
-    await puzzle1.run();
+    // await puzzle1.run();
+    await puzzle2.run({ 
+        example: true, 
+        mainProblem: false 
+    });
 
     console.log(kleur.cyan(`All puzzles ran in ${timer.time}.`));
 }
