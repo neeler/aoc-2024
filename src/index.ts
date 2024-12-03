2eda3393cf2f346ff8f335e12b025bd917d446db
Neel Rao <neelorao@gmail.com>
2024-12-02 21:41:19 -0800

    feat: puzzle 3

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 import kleur from 'kleur';
-import {
-    puzzle1,
-    puzzle2,
-    puzzle3,
-} from '~/puzzles';
+import { puzzle1, puzzle2, puzzle3 } from '~/puzzles';
 import { Timer } from '~/util/Timer';
 
-async function start() {    
+async function start() {
     const timer = new Timer();
 
     // await puzzle1.run();
     // await puzzle2.run();
-    await puzzle3.run({ 
-        example: true, 
-        mainProblem: false 
-    });
+    await puzzle3.run();
 
     console.log(kleur.cyan(`All puzzles ran in ${timer.time}.`));
 }
--- src/puzzles/puzzle3.ts
+++ src/puzzles/puzzle3.ts
@@ -5,10 +5,47 @@ export const puzzle3 = new Puzzle({
     parseInput: (fileData) => {
         return fileData.split('\n').filter((s) => s);
     },
-    part1: (data) => {
-        console.log(data);
+    part1: (lines) => {
+        const validCommandRegex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+        let total = 0;
+        lines.forEach((line) => {
+            const matches = [...line.matchAll(validCommandRegex)];
+            for (const [, first = '', second = ''] of matches) {
+                total +=
+                    Number.parseInt(first, 10) * Number.parseInt(second, 10);
+            }
+        });
+        return total;
     },
-    part2: (data) => {
-        //
+    part2: (lines) => {
+        let total = 0;
+        let mulEnabled = true;
+
+        const commandRegex =
+            /(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/g;
+
+        lines.forEach((line) => {
+            const matches = [...line.matchAll(commandRegex)];
+            for (const [
+                ,
+                ,
+                first = '',
+                second = '',
+                doCommand,
+                dontCommand,
+            ] of matches) {
+                if (doCommand) {
+                    mulEnabled = true;
+                } else if (dontCommand) {
+                    mulEnabled = false;
+                } else if (mulEnabled) {
+                    total +=
+                        Number.parseInt(first, 10) *
+                        Number.parseInt(second, 10);
+                }
+            }
+        });
+
+        return total;
     },
 });
