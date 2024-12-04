90288bebfe8ee060e4e3de2f7fe776cb1dec0138
Neel Rao <neelorao@gmail.com>
2024-12-03 21:35:39 -0800

    feat: setup puzzle 5

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import kleur from 'kleur';
-import { puzzle1, puzzle2, puzzle3, puzzle4 } from '~/puzzles';
+import {
+    puzzle1,
+    puzzle2,
+    puzzle3,
+    puzzle4,
+    puzzle5,
+} from '~/puzzles';
 import { Timer } from '~/util/Timer';
 
-async function start() {
+async function start() {    
     const timer = new Timer();
 
     // await puzzle1.run();
     // await puzzle2.run();
     // await puzzle3.run();
-    await puzzle4.run({
-        example: true,
-        mainProblem: true,
+    // await puzzle4.run();
+    await puzzle5.run({ 
+        example: true, 
+        mainProblem: false 
     });
 
     console.log(kleur.cyan(`All puzzles ran in ${timer.time}.`));
